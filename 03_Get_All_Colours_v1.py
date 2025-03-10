1ca6bfb860fdbfbac8f26ac35a0c4f5ed1a405a7
DaxtorT <110640402+DaxtorT@users.noreply.github.com>
2025-03-11 10:00:24 +1300

    V1 of Get_Colours (Retrieves colours from csv and picks 4)

--- 03_Get_All_Colours_v1.py
+++ 03_Get_All_Colours_v1.py
@@ -9,4 +9,27 @@ file.close()
 # Remove the first row of the csv
 all_colours.pop(0)
 
-print(all_colours)
+round_colours = []
+colour_scores = []
+
+
+# Loop until we have four colours with different scores
+while len(round_colours) < 4:
+
+    potential_colour = random.choice(all_colours)
+
+    # Get the score and check its not a duplicate
+    if potential_colour[1] not in colour_scores:
+        round_colours.append(potential_colour)
+        colour_scores.append(potential_colour[1])
+
+print(round_colours)
+print(colour_scores)
+
+# Change scores to integers
+int_scores = [int(x) for x in colour_scores]
+int_scores.sort()
+
+median = (int_scores[1] + int_scores[2]) / 2
+median = round(median)
+print("median", median)
